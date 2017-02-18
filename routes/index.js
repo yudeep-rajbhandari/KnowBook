1d2f7d713db376f5bc40c7fea598c1bf465cd497
yudeep-rajbhandari <endlessveins@gmail.com>
2017-02-18 21:49:34 +0545

    query re:2

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/test', function(req, res, next) {
-  res.json({message:"this is done"})
+  res.json({message:"this is done again"})
 });
 
 

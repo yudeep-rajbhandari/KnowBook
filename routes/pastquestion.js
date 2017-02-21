eab1cdd57122a177adfd446abae730fa65759798
yudeep-rajbhandari <endlessveins@gmail.com>
2017-02-21 23:16:14 +0545

    notes url

--- routes/pastquestion.js
+++ routes/pastquestion.js
@@ -13,6 +13,30 @@ router.get('/', function (req, res, next) {
 });
 
 
+router.get('/Requestsubject', function (req, res, next) {
+    // console.log(req.params.subjectid);
+    console.log("hello");
+    subjectModel.find({Faculties:req.query.Faculty,Semester:req.query.Semester})
+        .exec(function (err, data) {
+            var subjectIds=data.map(val=>{
+                return val._id;
+            })
+            model.find({Subjectid:{$in:subjectIds}}).populate('Subjectid').exec(function (err,data) {
+                if (err) {
+                    throw (err);
+                }
+                if (data) {
+                    res.status(200).json({ success: true, data: data })
+                    console.log(data);
+                }
+                else {
+                    next(err);
+                }
+            })
+
+
+        })
+})
 
 router.post('/addquestion', function (req, res, next) {
 
@@ -28,6 +52,26 @@ router.post('/addquestion', function (req, res, next) {
             next(err);
         }
     })
+})
+
+router.get('/getquestion', function (req, res, next) {
+    // console.log(req.params.subjectid);
+
+    model.find({})
+        .populate('Subjectid')
+        .exec(function (err, data) {
+
+            if (err) {
+                throw (err);
+            }
+            if (data) {
+                res.status(200).json({ success: true, data: data })
+                console.log(data);
+            }
+            else {
+                next(err);
+            }
+        })
 })
 router.get('/Requests/:subjectid2', function (req, res, next) {
     // console.log(req.params.subjectid);
